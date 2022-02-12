33835e700e841fd66e6a3d1b39876a95afbcc635
vinay <vinay@LAPTOP-GH7PUIOE>
2022-02-11 20:58:27 -0600

    First Commit...

---
+++ src/main/java/com/hms/HmsSeApplication.java
@@ -0,0 +1,13 @@
+package com.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsSeApplication.class, args);
+	}
+
+}
