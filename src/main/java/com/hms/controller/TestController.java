15cb79db76de51b4e95b2586e9dab3953ee7e24e
vinay <vinay@LAPTOP-GH7PUIOE>
2022-02-12 18:29:30 -0600

    changed to default...

--- src/main/java/com/hms/controller/TestController.java
+++ src/main/java/com/hms/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-	@RequestMapping(value="/index")
+	@RequestMapping(value="/")
 	public String index() {
 		return "index";
 	}
